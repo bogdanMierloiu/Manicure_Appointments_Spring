423d01be56f82707c98ac365766556142fe76e4c
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2022-11-25 13:20:03 +0200

    new customer details

---
+++ src/main/java/ro/musiclover/manicureappointments/model/customer/CustomerUpdateStatus.java
@@ -0,0 +1,28 @@
+package ro.musiclover.manicureappointments.model.customer;
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+@Data
+public class CustomerRequest {
+
+    private Integer id;
+
+    @NotBlank(message = "Invalid first name")
+    private String firstName;
+
+    @NotBlank(message = "Invalid last name")
+    private String lastName;
+
+    @NotBlank(message = "Invalid phone number")
+    private String phoneNumber;
+
+    @Past
+    private LocalDate birthDate;
+
+    @NotBlank
+    private String email;
+
+    private boolean isActive;
+}
