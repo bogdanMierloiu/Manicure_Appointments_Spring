edb31d194326bed1036e75c55f5385b3569860c5
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2022-12-21 23:22:13 +0200

    required for all inputs tags

--- target/generated-sources/annotations/ro/musiclover/manicureappointments/mapper/AppointmentMapperImpl.java
+++ target/generated-sources/annotations/ro/musiclover/manicureappointments/mapper/AppointmentMapperImpl.java
@@ -18,7 +18,7 @@ import ro.musiclover.manicureappointments.model.nails_services.NailsCareResponse
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-21T11:54:57+0200",
+    date = "2022-12-21T14:17:21+0200",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 18.0.2 (Amazon.com Inc.)"
 )
 @Component
